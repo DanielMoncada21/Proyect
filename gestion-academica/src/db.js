ae23c7cc399d7a7b2103e77446c053731a7b0198
DanielMoncada21 <moncadadaniel198@gmail.com>
2025-06-02 18:26:05 -0500

    subiendo

--- gestion-academica/src/db.js
+++ gestion-academica/src/db.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
@@ -9,7 +11,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error('Error al conectar a la base de datos:', err);
     return;
   }
   console.log('Conexión exitosa a la base de datos');
